421ac909e91530ec703b786222396f630a68af6e
abitmore <abitmore@users.noreply.github.com>
2015-09-24 05:24:43 +0800

    price feed: disable discount or extra fee

--- scripts/pricefeeds.py
+++ scripts/pricefeeds.py
@@ -423,8 +423,8 @@ if __name__ == "__main__":
                         }, 
                         "base": {
                           "asset_id": assets[asset]["id"], 
-                          #"amount": core_price
-                          "amount": int(core_price * 1.05) # 5% extra
+                          "amount": core_price
+                          #"amount": int(core_price * 1.05) # 5% extra
                         }
                       }, 
                       "core_exchange_rate": {
