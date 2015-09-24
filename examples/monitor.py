c74b6bfd77760d89ae468050b67f3e728ca0b751
Fabian Schuh <mail@xeroc.org>
2015-09-24 21:59:03 +0200

    documentation corrections and more descriptions

--- examples/monitor.py
+++ examples/monitor.py
@@ -3,6 +3,12 @@ import json
 from grapheneapi import GrapheneWebsocket, GrapheneWebsocketProtocol
 from graphenebase import Memo, PrivateKey, PublicKey
 
+""" RPC connection settings """
+host     = "localhost"
+port     = 8090
+user     = ""
+password = ""
+
 """ Account id to monitor """
 accountID = "2.6.69585"
 
@@ -72,13 +78,6 @@ class GrapheneMonitor(GrapheneWebsocketProtocol) :
                       memomsg))
 
 if __name__ == '__main__':
-
-    ## RPC connections
-    host     = "localhost"
-    port     = 8090
-    user     = ""
-    password = ""
-
     ## Monitor definitions
     protocol = GrapheneMonitor
     protocol.last_op = last_op ## last operation logged
