8c151a1e8a558ca6501d3c9d2e3fd0a104985aa6
Fabian Schuh <mail@xeroc.org>
2015-10-09 16:05:49 +0200

    Require Confirmation on price feed update. Requires reinstallation of pyhthon-graphenelib

--- grapheneapi/grapheneapi.py
+++ grapheneapi/grapheneapi.py
@@ -28,6 +28,25 @@ class GrapheneAPI(object) :
         self.username = username
         self.password = password
         self.headers  = {'content-type': 'application/json'}
+
+    def _confirm(self, question, default="yes"):
+        valid = {"yes": True, "y": True, "ye": True,
+                 "no": False, "n": False}
+        if default is None:    prompt = " [y/n] "
+        elif default == "yes": prompt = " [Y/n] "
+        elif default == "no":  prompt = " [y/N] "
+        else:
+            raise ValueError("invalid default answer: '%s'" % default)
+        while True:
+            sys.stdout.write(question + prompt)
+            choice = input().lower()
+            if default is not None and choice == '':
+                return valid[default]
+            elif choice in valid:
+                return valid[choice]
+            else:
+                sys.stdout.write("Please respond with 'yes' or 'no' "
+                                 "(or 'y' or 'n').\n")              
         
     """ Manual execute a command on API
     """
