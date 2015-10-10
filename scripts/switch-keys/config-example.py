17461b17983e5d6c7be9ab41c483dcd43aefcd1a
gileadmcgee <gileadmcgee@gmail.com>
2015-10-10 12:57:17 -0700

    adding failover script

---
+++ scripts/switch-keys/config-example.py
@@ -0,0 +1,5 @@
+witnessname = "dele-puppy"
+publickeys = ("GPH8Sj81ncf8PK5QwJZvhWsvwp6m4bXzTFvp4bKycNNFvHaJpZSnH", "GPH75xxKG4ZeztPpnhmFch99smunUWMvDy9mB6Le497vpAA3XUXaD")
+strictness = 1
+emergencykeys = 0
+
