bec2b168e2e6213f479db5d59222cd2821fca426
Scott Howard <showard314@gmail.com>
2015-12-09 14:38:45 -0500

    fix: no notices if no account is subscribed
    Without this, notices are dispatched for all objects to the None object even if no account is subscribed

--- grapheneapi/graphenewsprotocol.py
+++ grapheneapi/graphenewsprotocol.py
@@ -73,7 +73,7 @@ class GrapheneWebsocketProtocol(WebSocketClientProtocol):
             oid = notice["id"];
             if oid in self.database_callbacks_ids :
                 self.database_callbacks_ids[oid](self,notice)
-            if oid and self.accounts_callback :
+            if oid and self.accounts_callback != [None]:
                 self.accounts_callback[0](self,notice)
         except Exception as e :
             print('Error dispatching notice: %s' % str(e))
