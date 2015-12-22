566fda147a23c4d9aeca8914173f4fd845d8738f
Fabian Schuh <mail@xeroc.org>
2015-12-22 08:33:53 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup, find_packages
 
 setup(
       name='graphenelib',
-      version='v0.1',
+      version='v0.1.1',
       description='Python library for graphene-based blockchains',
       long_description=open('README.md').read(),
-      download_url = 'https://github.com/xeroc/python-graphenelib/tarball/v0.1',
+      download_url = 'https://github.com/xeroc/python-graphenelib/tarball/v0.1.1',
       author='Fabian Schuh',
       author_email='<Fabian@BitShares.eu>',
       maintainer='Fabian Schuh',
