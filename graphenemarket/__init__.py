4b645c15be82c8b3b24d09f432cc674c26d53144
Fabian Schuh <mail@xeroc.org>
2016-01-05 15:48:40 +0100

    Initial Market

---
+++ graphenemarket/__init__.py
@@ -0,0 +1,2 @@
+__all__ = ['graphenemarket']
+from .graphenemarket import GrapheneMarket
--- graphenemarket/graphenemarket.py
+++ graphenemarket/graphenemarket.py
@@ -1,9 +1,131 @@
-from math import log10
+from grapheneapi import GrapheneAPI, GrapheneWebsocketProtocol
+from datetime import datetime
+import time
 
-class market :
-    def __init__(self, client) :
-        raise Exception("This code is not tested. Don't use it!")
-        self.client = client ## pass rpc commands to the client
+
+class GrapheneMarket(GrapheneWebsocketProtocol) :
+    markets = {}
+    account = ""
+    wallet = None
+
+    def __init__(self, config) :
+        super().__init__()
+        self.wallet = GrapheneAPI(config.wallet_host,
+                                  config.wallet_port,
+                                  config.wallet_user,
+                                  config.wallet_password)
+
+    def formatTimeFromNow(self, secs):
+        return datetime.utcfromtimestamp(time.time() + int(secs)).strftime('%Y-%m-%dT%H:%M:%S')
+
+    """ Poloniex compatible API
+        #######################
+    """
+
+"""
+    def returnTicker(self):
+        r = {}
+        for market in self.markets :
+            print(self.markets)
+            m = self.markets[market]
+            marketHistory = client.rpc.get_market_history(
+                m["quote"], m["base"], 24 * 60 * 60)
+            orders = client.rpc.get_limit_orders(
+                m["quote"], m["base"], 1)
+            filled = self.rpc.get_fill_order_history(
+                m["quote"], m["base"], 1, api_id=2)
+            print(orders)
+            data = {}
+            data["last"]          = 0
+            data["lowestAsk"]     = orders[1]["quote"] / orders[1]["base"]
+            data["highestBid"]    = orders[0]["quote"] / orders[1]["base"]
+            data["percentChange"] = 0
+            data["baseVolume"]    = marketHistory[0]["base_volume"]
+            data["quoteVolume"]   = marketHistory[0]["quote_volume"]
+            r.update({market : data})
+        return r
+
+    def return24Volume(self):
+        pass
+
+    def returnOrderBook(self, currencyPair):
+        pass
+
+    def returnMarketTradeHistory(self, currencyPair):
+        pass
+
+    def returnBalances(self):
+        # Returns all of your balances.
+        # Outputs:
+        # {"BTC":"0.59098578","LTC":"3.31117268", ... }
+        pass
+
+    # Returns your open orders for a given market, specified by the "currencyPair" POST parameter, e.g. "BTC_XCP"
+    # Inputs:
+    # currencyPair  The currency pair e.g. "BTC_XCP"
+    # Outputs:
+    # orderNumber   The order number
+    # type          sell or buy
+    # rate          Price the order is selling or buying at
+    # Amount        Quantity of order
+    # total         Total value of order (price * quantity)
+    def returnOpenOrders(self, currencyPair):
+        return self.api_query('returnOpenOrders',{"currencyPair":currencyPair})
+
+    # Returns your trade history for a given market, specified by the "currencyPair" POST parameter
+    # Inputs:
+    # currencyPair  The currency pair e.g. "BTC_XCP"
+    # Outputs:
+    # date          Date in the form: "2014-02-19 03:44:59"
+    # rate          Price the order is selling or buying at
+    # amount        Quantity of order
+    # total         Total value of order (price * quantity)
+    # type          sell or buy
+    def returnTradeHistory(self, currencyPair):
+        return self.api_query('returnTradeHistory', {"currencyPair" : currencyPair})
+
+    # Places a buy order in a given market. Required POST parameters are "currencyPair", "rate", and "amount". If successful, the method will return the order number.
+    # Inputs:
+    # currencyPair  The curreny pair
+    # rate          price the order is buying at
+    # amount        Amount of coins to buy
+    # Outputs:
+    # orderNumber   The order number
+    def buy(self, currencyPair, rate, amount):
+        return self.api_query('buy', {"currencyPair" : currencyPair, "rate" : rate , "amount" : amount})
+
+    # Places a sell order in a given market. Required POST parameters are "currencyPair", "rate", and "amount". If successful, the method will return the order number.
+    # Inputs:
+    # currencyPair  The curreny pair
+    # rate          price the order is selling at
+    # amount        Amount of coins to sell
+    # Outputs:
+    # orderNumber   The order number
+    def sell(self, currencyPair, rate, amount):
+        return self.api_query('sell', {"currencyPair" : currencyPair, "rate" : rate, "amount":amount})
+
+    # Cancels an order you have placed in a given market. Required POST parameters are "currencyPair" and "orderNumber".
+    # Inputs:
+    # currencyPair  The curreny pair
+    # orderNumber   The order number to cancel
+    # Outputs:
+    # succes        1 or 0
+    def cancel(self, currencyPair, orderNumber):
+        return self.api_query('cancelOrder', {"currencyPair":currencyPair,"orderNumber":orderNumber})
+
+    # Immediately places a withdrawal for a given currency, with no email confirmation. In order to use this method, the withdrawal privilege must be enabled for your API key. Required POST parameters are "currency", "amount", and "address". Sample output: {"response":"Withdrew 2398 NXT."}
+    # Inputs:
+    # currency      The currency to withdraw
+    # amount        The amount of this coin to withdraw
+    # address       The withdrawal address
+    # Outputs:
+    # response      Text containing message about the withdrawal
+    def withdraw(self, currency, amount, address):
+        return self.api_query('withdraw',{"currency":currency, "amount":amount, "address":address})
+
+    " Other Calls
+        #######################
+    "
 
     def get_asset_id(self, asset):
         return self.client.blockchain_get_asset(asset)["result"]["id"]
@@ -19,230 +141,34 @@ class market :
 
     def get_lowest_bid(self, asset1, asset2):
         return float(self.client.blockchain_market_order_book(asset1, asset2)["result"][0][0]["market_index"]["order_price"]["ratio"])
-        
-    def cancel_bids_less_than(self, account, quote, base, price):
-        cancel_args = self.get_bids_less_than(account, quote, base, price)[0]
-        response = self.client.batch("wallet_market_cancel_order", cancel_args)
-        return cancel_args
-
-    def get_median(self, asset):
-        response = self.client.blockchain_get_feeds_for_asset(asset)
-        feeds = response["result"]
-        return feeds[len(feeds)-1]["median_price"]
-
-    def cancel_bids_out_of_range(self, account, quote, base, price, tolerance):
-        cancel_args = self.get_bids_out_of_range(account, quote, base, price, tolerance)[0]
-        response = self.client.request("batch", ["wallet_market_cancel_order", cancel_args])
-        return cancel_args
-
-    def cancel_asks_out_of_range(self, account, quote, base, price, tolerance):
-        cancel_args = self.get_asks_out_of_range(account, quote, base, price, tolerance)[0]
-        response = self.client.request("batch", ["wallet_market_cancel_order", cancel_args])
-        return cancel_args
 
     def get_balance(self, account, asset):
-        asset_id = self.get_asset_id(asset) 
-        response = self.client.wallet_account_balance(account, asset)
-        if not response:
-            return 0
-        if "result" not in response or response["result"] == None or not len(response["result"]):
-            return 0
-        asset_array = response["result"][0][1]
-        amount = 0
-        for item in asset_array:
-            if item[0] == asset_id:
-                amount = float(item[1])
-                return amount / self.get_precision(asset)
-        return 0
+        pass
 
     def cancel_all_orders(self, account, quote, base):
-        cancel_args = self.get_all_orders(account, quote, base)
-        for i in cancel_args[0] :
-            response = self.client.wallet_market_cancel_order(i)
-        return cancel_args[1]
+        pass
 
     def ask_at_market_price(self, name, amount, base, quote, confirm=False) :
-        response       = self.client.blockchain_market_order_book(quote, base)
-        quotePrecision = self.get_precision(quote)
-        basePrecision  = self.get_precision(base)
-        orders = []
-        for order in response["result"][0]: # bid orders
-            order_price  = float(order["market_index"]["order_price"]["ratio"])*(basePrecision / quotePrecision) 
-            order_amount = float(order["state"]["balance"]/quotePrecision) / order_price  # denoted in BASE
-            if amount >= order_amount : # buy full amount
-              orders.append([name, order_amount, base, order_price, quote])
-              amount -= order_amount
-            elif amount < order_amount: # partial
-              orders.append([name, amount, base, order_price, quote])
-              break
-        for o in orders :
-            print( "Selling %15.8f %s for %12.8f %s @ %12.8f" %(o[1], o[2], o[1]*o[3], o[4], o[3]) )
-        if not confirm or self.client.query_yes_no( "I dare you confirm the orders above: ") :
-           for o in orders :
-               self.submit_ask(o[0], o[1], o[2], o[3], o[4])
+        pass
 
     def bid_at_market_price(self, name, amount, base, quote, confirm=False) :
-        response       = self.client.blockchain_market_order_book(quote, base)
-        quotePrecision = self.get_precision(quote)
-        basePrecision  = self.get_precision(base)
-        orders = []
-        for order in response["result"][1]: # ask orders
-            order_price  = float(order["market_index"]["order_price"]["ratio"])*(basePrecision / quotePrecision) 
-            order_amount = float(order["state"]["balance"]/quotePrecision) / order_price  # denoted in BASE
-            if amount >= order_amount : # buy full amount
-              orders.append([name, order_amount, base, order_price, quote])
-              amount -= order_amount
-            elif amount < order_amount: # partial
-              orders.append([name, amount, base, order_price, quote])
-              break
-        for o in orders :
-            print( "Selling %15.8f %s for %12.8f %s @ %12.8f" %(o[1], o[2], o[1]*o[3], o[4], o[3]) )
-        if not confirm or self.client.query_yes_no( "I dare you confirm the orders above: ") :
-           for o in orders :
-               self.submit_bid(o[0], o[1], o[2], o[3], o[4])
-
-    def ask_limit(self, name, amount, base, quote, price_limit, confirm=False) :
-        print("Buying orders with price limit: %f %s/%s" % (price_limit, base, quote))
-        response       = self.client.blockchain_market_list_bids(quote, base)
-        quotePrecision = self.get_precision(quote)
-        basePrecision  = self.get_precision(base)
-        orders = []
-        for order in response["result"] :
-            order_price  = float(order["market_index"]["order_price"]["ratio"])*(basePrecision / quotePrecision) 
-            order_amount = float(order["state"]["balance"]/quotePrecision) / order_price  # denoted in BASE
-            if order_price < price_limit :
-               orders.append([name, amount, base, price_limit, quote])
-               break
-            else:
-               if amount >= order_amount : # buy full amount
-                 orders.append([name, order_amount, base, order_price, quote])
-                 amount -= order_amount
-               elif amount < order_amount: # partial
-                 orders.append([name, amount, base, order_price, quote])
-                 break
-        for o in orders :
-            print( "Buying %15.8f %s for %12.8f %s @ %12.8f" %(o[1], o[2], o[1]*o[3], o[4], o[3]) )
-        if not confirm or self.client.query_yes_no( "I dare you confirm the orders above: ") :
-           for o in orders :
-               amount = str('%.*f' %(int(log10(quotePrecision)), o[1]))
-               self.submit_ask(o[0], amount, o[2], o[3], o[4])
-
-    def bid_limit(self, name, amount, base, quote, price_limit, confirm=False) :
-        print("Sell orders with price limit: %f %s/%s" % (price_limit, base, quote))
-        response       = self.client.blockchain_market_list_asks(quote, base)
-        quotePrecision = self.get_precision(quote)
-        basePrecision  = self.get_precision(base)
-        orders = []
-        for order in response["result"] :
-            order_price  = float(order["market_index"]["order_price"]["ratio"])*(basePrecision / quotePrecision) 
-            order_amount = float(order["state"]["balance"])/order_price/quotePrecision  # denoted in BASE
-            if order_price > price_limit :
-               orders.append([name, amount, base, price_limit, quote])
-               break
-            else:
-               if amount >= order_amount : # buy full amount
-                 orders.append([name, order_amount, base, order_price, quote])
-                 amount -= order_amount
-               elif amount < order_amount: # partial
-                 orders.append([name, amount, base, order_price, quote])
-                 break
-        for o in orders :
-            print( "Buying %15.8f %s for %12.8f %s @ %12.8f" %(o[1], o[2], o[1]*o[3], o[4], o[3]) )
-        if not confirm or self.client.query_yes_no( "I dare you confirm the orders above: ") :
-           for o in orders :
-               amount = str('%.*f' %(int(log10(quotePrecision)), o[1]))
-               self.submit_bid(o[0], amount, o[2], o[3], o[4])
-
+        pass
 
     def submit_bid(self, account, amount, quote, price, base):
-        print("%s submitted a bid" % account)
-        self.client.bid(account, amount, quote, price, base)
+        pass
 
     def submit_ask(self, account, amount, quote, price, base):
-        print("%s submitted an ask" % account)
-        self.client.ask(account, amount, quote, price, base)
+        pass
 
     def get_bids_less_than(self, account, quote, base, price):
-        quotePrecision = self.get_precision( quote )
-        basePrecision = self.get_precision( base )
-        response = self.client.wallet_market_order_list(quote, base, -1, account)
-        order_ids = []
-        quote_shares = 0
-        if "result" not in response or response["result"] == None:
-            return [[], 0]
-        for pair in response["result"]:
-            order_id = pair[0]
-            item = pair[1]
-            if item["type"] == "bid_order":
-                if float(item["market_index"]["order_price"]["ratio"])* (basePrecision / quotePrecision) < price:
-                    order_ids.append(order_id)
-                    quote_shares += int(item["state"]["balance"])
-                    print("%s canceled an order: %s" % (account, str(item)))
-        cancel_args = [item for item in order_ids]
-        return [cancel_args, float(quote_shares) / quotePrecision]
-
-    def get_bids_out_of_range(self, account, quote, base, price, tolerance):
-        quotePrecision = self.get_precision( quote )
-        basePrecision = self.get_precision( base )
-        response = self.client.wallet_market_order_list(quote, base, -1, account)
-        order_ids = []
-        quote_shares = 0
-        if "result" not in response or response["result"] == None:
-           return [[], 0]
-        for pair in response["result"]:
-            order_id = pair[0]
-            item = pair[1]
-            if item["type"] == "bid_order":
-                if abs(price - float(item["market_index"]["order_price"]["ratio"]) * (basePrecision / quotePrecision)) > tolerance:
-                    order_ids.append(order_id)
-                    quote_shares += int(item["state"]["balance"])
-                    print("%s canceled an order: %s" % (account, str(item)))
-        cancel_args = [item for item in order_ids]
-        return [cancel_args, float(quote_shares) / quotePrecision]
-
-    def get_asks_out_of_range(self, account, quote, base, price, tolerance):
-        quotePrecision = self.get_precision( quote )
-        basePrecision = self.get_precision( base )
-        response = self.client.wallet_market_order_list(quote, base, -1, account)
-        order_ids = []
-        base_shares = 0
-        if "result" not in response or response["result"] == None:
-           return [[], 0]
-        for pair in response["result"]:
-            order_id = pair[0]
-            item = pair[1]
-            if item["type"] == "ask_order":
-                if abs(price - float(item["market_index"]["order_price"]["ratio"]) * (basePrecision / quotePrecision)) > tolerance:
-                    order_ids.append(order_id)
-                    base_shares += int(item["state"]["balance"])
-        cancel_args = [item for item in order_ids]
-        return [cancel_args, base_shares / basePrecision]
+        pass
 
     def get_all_orders(self, account, quote, base):
-        response = self.client.wallet_market_order_list(quote, base, -1, account)
-        order_ids = []
-        orders = []
-        if "result" in response :
-           for item in response["result"]:
-               order_ids.append(item[0])
-               orders.append(item[1])
-           orderids = [item for item in order_ids]
-           return [ orderids, orders ]
-        return
+        pass
 
-    def get_last_fill (self, quote, base):
-        last_fill = -1
-        response = self.client.blockchain_market_order_history(quote, base, 0, 1)
-        for order in response["result"]:
-            last_fill = float(order["ask_price"]["ratio"]) 
-        return last_fill
+    def get_last_fill(self, quote, base):
+        pass
 
     def get_price(self, quote, base):
-        response = self.client.blockchain_market_order_book(quote, base, 1)
-        order = response["result"]
-        quotePrecision = self.get_precision(quote)
-        basePrecision  = self.get_precision(base)
-        lowest_bid  = float(order[0][0]["market_index"]["order_price"]["ratio"])*(basePrecision / quotePrecision)
-        highest_ask = float(order[1][0]["market_index"]["order_price"]["ratio"])*(basePrecision / quotePrecision)
-        return (lowest_bid+highest_ask)/2
-
+        pass
+"""
