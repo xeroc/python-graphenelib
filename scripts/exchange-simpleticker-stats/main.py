7fd49a6c8a8a156268e8ec7883a3c8fef43995a5
Fabian Schuh <mail@xeroc.org>
2016-01-15 10:27:41 +0100

    [ScriptSimpleTicker] Fixed output

--- scripts/exchange-simpleticker-stats/main.py
+++ scripts/exchange-simpleticker-stats/main.py
@@ -11,7 +11,7 @@ class Config():
     witness_user          = ""
     witness_password      = ""
 
-    watch_markets         = ["USD_BTS", "CNY_BTS", "GOLD_BTS", "SILVER_BTS"]
+    watch_markets         = ["USD_BTS", "CNY_BTS", "GOLD_BTS", "SILVER_BTS", "EUR_BTS"]
     market_separator      = "_"
     account               = "xeroc"
 
@@ -20,17 +20,17 @@ if __name__ == '__main__':
     ticker = dex.returnTicker()
     for a in ticker:
         premium = math.fabs((ticker[a]["last"] / ticker[a]["settlement_price"] - 1) * 100)
-        premium_bid = math.fabs((ticker[a]["highestBid"] / ticker[a]["settlement_price"] - 1) * 100)
-        premium_ask = math.fabs((ticker[a]["lowestAsk"] / ticker[a]["settlement_price"] - 1) * 100)
-        premium = math.fabs((ticker[a]["last"] / ticker[a]["settlement_price"] - 1) * 100)
+        premium_bid = ((ticker[a]["settlement_price"] / ticker[a]["highestBid"] - 1) * 100)
+        premium_ask = ((ticker[a]["settlement_price"] / ticker[a]["lowestAsk"] - 1) * 100)
+        premium = math.fabs((ticker[a]["settlement_price"] / ticker[a]["last"] - 1) * 100)
         price_mid = (ticker[a]["highestBid"] + ticker[a]["lowestAsk"]) / 2.0
         spread = math.fabs(ticker[a]["highestBid"] - ticker[a]["lowestAsk"]) / price_mid * 100
         cer_premium = (ticker[a]["core_exchange_rate"] / ticker[a]["settlement_price"] - 1) * 100
 
         print("\n%s" % a)
         print("=" * len(a))
-        print(" - Trade Premium: %5.3f%%" % premium)
-        print(" - Bid Order Premium: %5.3f%%" % premium_bid)
-        print(" - Ask Order Premium: %5.3f%%" % premium_ask)
-        print(" - Spread: %5.3f%%" % spread)
-        print(" - CER premium: %5.2f%%" % cer_premium)
+        print(" - Last Trade Premium: %+7.3f%%" % premium)
+        print(" - Ask Order Premium:  %+7.3f%%" % premium_bid)
+        print(" - Bid Order Premium:  %+7.3f%%" % premium_ask)
+        print(" - Spread:             %+7.3f%%" % spread)
+        print(" - CER premium:        %+7.3f%%" % cer_premium)
