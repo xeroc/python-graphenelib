df7ef7fc6b983cc1db31d9b9f7e346f259b7e7cb
Fabian Schuh <mail@xeroc.org>
2016-03-24 10:14:54 +0100

    [Example] Hardfork time fix

--- examples/hardfork_time.py
+++ examples/hardfork_time.py
@@ -6,7 +6,7 @@ dtime = datetime.datetime.now()
 
 # Add delay
 # class datetime.timedelta([days[, seconds[, microseconds[, milliseconds[, minutes[, hours[, weeks]]]]]]])
-dtime = dtime + datetime.timedelta(60 * 60 * 2)
+dtime = dtime + datetime.timedelta(0, 60 * 60 * 12)
 
 # print timestamp
-print(time.mktime(dtime.timetuple()))
+print(int(time.mktime(dtime.timetuple())))
