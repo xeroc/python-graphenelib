7cc55d7d6e19388e8225b66c47e5437b9748e582
iHashFury <iPerky@users.noreply.github.com>
2016-06-13 13:55:26 +0200

    Push 2 (#3)
    * Possible fix for issue #33, small config-example fix
    
    * 2nd try at a fix for smartcoins not priced in the core_asset
    
    * small comment fix and changed a few variable names
    
    * updated variable names according to suggestions, moved repeating code into a for loop, added commented out example code for altcap.
    
    * removed debugging print statement

--- scripts/pricefeeds/config-example.py
+++ scripts/pricefeeds/config-example.py
@@ -63,7 +63,7 @@ change_max                   = 5.0       # Percentage of price change to cause a
 ################################################################################
 _all_assets = ["BTC", "SILVER", "GOLD", "TRY", "SGD", "HKD", "NZD",
                "CNY", "MXN", "CAD", "CHF", "AUD", "GBP", "JPY", "EUR", "USD",
-               "KRW"]  # "SHENZHEN", "HANGSENG", "NASDAQC", "NIKKEI", "RUB", "SEK"
+               "KRW"]  # "SHENZHEN", "HANGSENG", "NASDAQC", "NIKKEI", "RUB", "SEK", "ALTCAP", "ALTCAP.X"
 _bases = ["CNY", "USD", "BTC", "EUR", "HKD", "JPY"]
 
 asset_config = {"default" : {  # DEFAULT BEHAVIOR
@@ -138,6 +138,24 @@ asset_config = {"default" : {  # DEFAULT BEHAVIOR
                 "TUSD" : {
                     "maximum_short_squeeze_ratio"   : 1001
                 },
+                # "ALTCAP" : {
+                #    "metric" : "weighted",
+                #    "sources" : ["coincap",
+                #                 "coinmarketcap",
+                #                 "poloniex",
+                #                 "ccedk",
+                #                 "bittrex",
+                #                 "btc38",]
+                #},
+                #"ALTCAP.X" : {
+                #    "metric" : "weighted",
+                #    "sources" : ["coincap",
+                #                 "coinmarketcap",
+                #                 "poloniex",
+                #                 "ccedk",
+                #                 "bittrex",
+                #                 "btc38",]
+                #},
                }
 
 # Other assets that are derived or something else.
@@ -202,12 +220,12 @@ feedSources["huobi"]    = feedsources.Huobi(allowFailure=True, quotes=["BTC"], b
 #                                                                 bases=["USD"]) # only USD with free subscription
 #feedSources["coinmarketcap"]    = feedsources.CoinmarketcapAltcap(quotes=["ALTCAP", "ALTCAP.X"],
 #                                                                    quoteName={"ALTCAP":"ALTCAP", "ALTCAP.X":"ALTCAP.X"},
-#                                                                    bases=["BTS"],
+#                                                                    bases=["BTC"],
 #                                                                    allowFailure=True)
 #
 #feedSources["coincap"]    = feedsources.CoincapAltcap(quotes=["ALTCAP", "ALTCAP.X"],
 #                                                                    quoteName={"ALTCAP":"ALTCAP", "ALTCAP.X":"ALTCAP.X"},
-#                                                                    bases=["BTS"],
+#                                                                    bases=["BTC"],
 #                                                                    allowFailure=True)
 #feedSources["fixer"] = feedsources.Fixer(allowFailure=True, quotes=["EUR", "JPY", "SEK", "CNY"], bases=["EUR", "USD", "CNY"]) # more available
 #feedSources["bitcoinvenezuela"] = feedsources.BitcoinVenezuela(allowFailure=True, quotes=["EUR", "USD", "VEF", "ARS", "BTC", "LTC"], bases=["BTC", "LTC", "USD"])
