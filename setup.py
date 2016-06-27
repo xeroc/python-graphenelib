8e938ed1dfc7a31672fa87ab68b81996e3a61492
Fabian Schuh <mail@xeroc.org>
2016-06-27 11:52:56 +0200

    version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except LookupError:
     ascii = codecs.lookup('ascii')
     codecs.register(lambda name, enc=ascii: {True: enc}.get(name == 'mbcs'))
 
-VERSION = '0.4.1'
+VERSION = '0.4.2'
 
 setup(name='graphenelib',
       version=VERSION,
@@ -31,13 +31,16 @@ setup(name='graphenelib',
                 "grapheneextra",
                 "grapheneexchange",
                 ],
-      install_requires=["autobahn>=0.12.1",
-                        "requests>=2.9.1",
-                        "pycrypto>=2.6.1",
-                        "scrypt>=0.7.1",
-                        "ecdsa>=0.13",
-                        "websocket-client>=0.37.0",
-                        "unidecode",
+      install_requires=["autobahn>=0.14",
+                        "ecdsa==0.13",
+                        "graphenelib==0.4.2",
+                        "pycrypto==2.6.1",
+                        "requests==2.10.0",
+                        "scrypt==0.7.1",
+                        "six==1.10.0",
+                        "txaio==2.5.1",
+                        "Unidecode==0.4.19",
+                        "websocket-client==0.37.0",
                         ],
       classifiers=['License :: OSI Approved :: MIT License',
                    'Operating System :: OS Independent',
