a51dcbb34a56a2b123f80b3699f8c61d01099510
Fabian Schuh <mail@xeroc.org>
2016-07-11 10:21:58 +0200

    [setup] develop version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except LookupError:
     ascii = codecs.lookup('ascii')
     codecs.register(lambda name, enc=ascii: {True: enc}.get(name == 'mbcs'))
 
-VERSION = '0.4.2'
+VERSION = 'develop'
 
 setup(name='graphenelib',
       version=VERSION,
