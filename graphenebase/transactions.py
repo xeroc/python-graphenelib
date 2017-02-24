c35c11fb546123d0aada37fe7d7ab1829a6fa9f0
Fabian Schuh <Fabian@chainsquad.com>
2017-02-24 11:42:44 +0100

    Revert "[TaPOS] link to the last irreversible block instead of headblock"
    This reverts commit 05cec8e450a09fd0d3fa2b40860760f7bff0c125.

--- graphenebase/transactions.py
+++ graphenebase/transactions.py
@@ -21,7 +21,7 @@ def getBlockParams(ws):
         witness node!
     """
     dynBCParams = ws.get_dynamic_global_properties()
-    ref_block_num = dynBCParams["last_irreversible_block_num"] & 0xFFFF
+    ref_block_num = dynBCParams["head_block_number"] & 0xFFFF
     ref_block_prefix = struct.unpack_from("<I", unhexlify(dynBCParams["head_block_id"]), 4)[0]
     return ref_block_num, ref_block_prefix
 
