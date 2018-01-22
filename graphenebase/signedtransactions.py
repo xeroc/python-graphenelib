d1160acbf8e2726f500a91501df24326665a27df
Fabian Schuh <Fabian@chainsquad.com>
2018-01-22 14:20:29 +0100

    [txid] allow to derive the tx id from signed tx objects

--- graphenebase/signedtransactions.py
+++ graphenebase/signedtransactions.py
@@ -71,6 +71,24 @@ class Signed_Transaction(GrapheneObject):
                 ('signatures', kwargs['signatures']),
             ]))
 
+    @property
+    def id(self):
+        """ The transaction id of this transaction
+        """
+        # Store signatures temporarily since they are not part of
+        # transaction id
+        sigs = self.data["signatures"]
+        self.data.pop("signatures", None)
+
+        # Generage Hash of the seriliazed version
+        h = hashlib.sha256(bytes(self)).digest()
+
+        # recover signatures
+        self.data["signatures"] = sigs
+
+        # Return properly truncated tx hash
+        return hexlify(h[:20]).decode("ascii")
+
     def getOperationKlass(self):
         return Operation
 
