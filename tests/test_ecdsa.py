8add73efb04fa570b5df588874b0e3a967ffd3b3
Fabian Schuh <mail@xeroc.org>
2018-02-20 19:31:36 +0100

    fix unittest

--- tests/test_ecdsa.py
+++ tests/test_ecdsa.py
@@ -1,3 +1,4 @@
+import pytest
 import unittest
 from graphenebase.ecdsasig import (
     sign_message,
@@ -10,6 +11,9 @@ wif = "5J4KCbg1G3my9b9hCaQXnHSm6vrwW9xQTJS6ZciW2Kek7cCkCEk"
 
 class Testcases(unittest.TestCase):
 
+    # Ignore warning:
+    # https://www.reddit.com/r/joinmarket/comments/5crhfh/userwarning_implicit_cast_from_char_to_a/
+    @pytest.mark.filterwarnings()
     def test_sign_message(self):
         signature = sign_message("Foobar", wif)
         self.assertTrue(verify_message("Foobar", signature))
