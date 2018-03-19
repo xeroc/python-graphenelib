145221589f1016b4508060279a20b7abee49702e
Fabian Schuh <Fabian@chainsquad.com>
2018-03-19 11:11:03 +0100

    fix ecdsa test

--- tests/test_ecdsa.py
+++ tests/test_ecdsa.py
@@ -1,8 +1,12 @@
 import pytest
 import unittest
 from binascii import hexlify, unhexlify
-import graphenebase.ecdsa as ecdsa
 from graphenebase.account import PrivateKey, PublicKey, Address
+from graphenebase.ecdsa import (
+    sign_message,
+    verify_message
+)
+
 
 wif = "5J4KCbg1G3my9b9hCaQXnHSm6vrwW9xQTJS6ZciW2Kek7cCkCEk"
 
@@ -21,7 +25,7 @@ class Testcases(unittest.TestCase):
     def test_sign_message_cryptography(self):
         if not ecdsa.CRYPTOGRAPHY_AVAILABLE:
             return
-        ecdsa.SECP256K1_MODULE = "cryptography"        
+        ecdsa.SECP256K1_MODULE = "cryptography"
         pub_key = bytes(repr(PrivateKey(wif).pubkey), "latin")
         signature = ecdsa.sign_message("Foobar", wif)
         pub_key_sig = ecdsa.verify_message("Foobar", signature)
@@ -30,7 +34,7 @@ class Testcases(unittest.TestCase):
     def test_sign_message_secp256k1(self):
         if not ecdsa.SECP256K1_AVAILABLE:
             return
-        ecdsa.SECP256K1_MODULE = "secp256k1"      
+        ecdsa.SECP256K1_MODULE = "secp256k1"
         pub_key = bytes(repr(PrivateKey(wif).pubkey), "latin")
         signature = ecdsa.sign_message("Foobar", wif)
         pub_key_sig = ecdsa.verify_message("Foobar", signature)
