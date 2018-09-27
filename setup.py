0cf4d892b1f5d1b02efdbe90faf63ef18f1a0aea
Fabian Schuh <xeroc@chainsquad.com>
2018-09-27 09:41:01 +0200

    version bump

--- setup.py
+++ setup.py
@@ -11,19 +11,20 @@ except LookupError:
     ascii = codecs.lookup('ascii')
     codecs.register(lambda name, enc=ascii: {True: enc}.get(name == 'mbcs'))
 
-VERSION = '0.6.8'
+VERSION = '1.0.0'
+URL = "https://github.com/xeroc/python-graphenelib"
 
 setup(
     name='graphenelib',
     version=VERSION,
     description='Python library for graphene-based blockchains',
     long_description=open('README.md').read(),
-    download_url='https://github.com/xeroc/python-graphenelib/tarball/' + VERSION,
+    download_url='{}/tarball/{}'.format(URL, VERSION),
     author='Fabian Schuh',
     author_email='Fabian@chainsquad.com',
     maintainer='Fabian Schuh',
     maintainer_email='Fabian@chainsquad.com',
-    url='http://www.github.com/xeroc/python-graphenelib',
+    url=URL,
     keywords=[
         'graphene',
         'api',
@@ -36,15 +37,13 @@ setup(
         "graphenebase",
         "graphenestorage",
     ],
-    install_requires=[
-        open("requirements.txt").readlines()
-    ],
     classifiers=['License :: OSI Approved :: MIT License',
                  'Operating System :: OS Independent',
                  'Programming Language :: Python :: 3',
-                 'Development Status :: 3 - Alpha',
+                 'Development Status :: 5 - Production/Stable',
                  'Intended Audience :: Developers',
                  ],
+    install_requires=open("requirements.txt").readlines(),
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     include_package_data=True,
