335e59d2e79cc50504386409ed11913bfe384b5a
Fabian Schuh <mail@xeroc.org>
2018-10-11 13:08:03 +0200

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except LookupError:
     ascii = codecs.lookup('ascii')
     codecs.register(lambda name, enc=ascii: {True: enc}.get(name == 'mbcs'))
 
-VERSION = '1.0.0'
+VERSION = '1.0.1rc2'
 URL = "https://github.com/xeroc/python-graphenelib"
 
 setup(
