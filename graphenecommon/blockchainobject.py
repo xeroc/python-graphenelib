3da58c39a9eb3c909da4ab7c45cbba5e2d6dca46
Fabian Schuh <Fabian@chainsquad.com>
2018-12-20 09:48:02 +0100

    Documentation for blockchainobject.py

--- graphenecommon/blockchainobject.py
+++ graphenecommon/blockchainobject.py
@@ -4,6 +4,20 @@ from .instance import AbstractBlockchainInstanceProvider
 
 
 class ObjectCache(dict):
+    """ This class implements an object/dict cache that comes with an
+        expiration. Expired items are removed from the cache.
+
+        The class implements/extends:
+
+         * __setitem__()
+         * __getitem__()
+         * __contains__
+         * __str__()
+         * get()
+
+        and provides a method to define the default expiration time.
+
+    """
     def __init__(self, initial_data={}, default_expiration=10, no_overwrite=False):
         dict.__init__(self, initial_data)
 
@@ -29,7 +43,10 @@ class ObjectCache(dict):
             value = dict.__getitem__(self, key)
             return value["data"]
 
-    def get(self, key, default):
+    def get(self, key, default=None):
+        """ Returns an element from the cache if available, else returns
+            the value provided as default or None
+        """
         if key in self:
             return self[key]
         else:
@@ -40,7 +57,10 @@ class ObjectCache(dict):
             value = dict.__getitem__(self, key)
             if datetime.utcnow() < value["expires"]:
                 return True
-        return False
+            else:
+                # Remove from cache
+                dict.pop(self, key, None)
+                return False
 
     def __str__(self):
         return "ObjectCache(n={}, default_expiration={})".format(
@@ -48,10 +68,15 @@ class ObjectCache(dict):
         )
 
     def set_expiration(self, expiration):
+        """ Set new default expiration time in seconds (default: 10s)
+        """
         self.default_expiration = expiration
 
 
 class Caching:
+    """ This class implements a few common methods that are used to
+        either cache lists or dicts
+    """
     def _store_item(self, key=None):
         if key is None and dict.__contains__(self, "id"):
             self._cache[self.get("id")] = self
@@ -65,9 +90,13 @@ class Caching:
         self._cached = True
 
     def incached(self, id):
+        """ Is an element cached?
+        """
         return id in self._cache
 
     def getfromcache(self, id):
+        """ Get an element from the cache explicitly
+        """
         return self._cache.get(id, None)
 
     def __getitem__(self, key):
@@ -76,6 +105,9 @@ class Caching:
         return dict.__getitem__(self, key)
 
     def items(self):
+        """ This overwrites items() so that refresh() is called it the
+            object is not already fetched
+        """
         if not self._cached:
             self.refresh()
         return dict.items(self)
@@ -90,20 +122,26 @@ class Caching:
 
     @classmethod
     def clear_cache(cls):
+        """ Clear/Reset the entire Cache
+        """
         cls._cache = ObjectCache()
 
     __str__ = __repr__
 
 
 class BlockchainObjects(list, Caching):
+    """ This class is used internally to store **lists** of objects and
+        deal with the cache and indexing thereof.
+    """
     _cache = ObjectCache()
     identifier = None
 
-    @property
-    def _cache_key(self):
-        key = self.__class__.__name__
-        key += self.identifier or ""
-        return key
+    def refresh(self, *args, **kwargs):
+        """ Interface that needs to be implemented. This method is
+            called when an object is requested that has not yet been
+            fetched/stored
+        """
+        raise NotImplementedError
 
     def __init__(self, *args, **kwargs):
         key = self._cache_key
@@ -113,12 +151,30 @@ class BlockchainObjects(list, Caching):
             if kwargs.get("refresh", True):
                 self.refresh(*args, **kwargs)
 
+    @property
+    def _cache_key(self):
+        key = self.__class__.__name__
+        key += self.identifier or ""
+        return key
+
     def store(self, data, *args, **kwargs):
+        """ Cache the list
+
+            :param list data: List of objects to cache
+        """
         list.__init__(self, data)
+        # We do not allow to provide a separate key as we do for
+        # a BlockchainObject (without s). The reson is that we store
+        # lists and do not need to be able to query for names, ids, or
+        # other objects
         self._store_items(self._cache_key)
 
-    def refresh(self, *args, **kwargs):
-        raise NotImplementedError
+    @classmethod
+    def cache_objects(cls, data, key=None):
+        """ This classmethod allows to feed multiple objects into the
+            cache is is mostly used for testing
+        """
+        return cls._import(cls, data, key)
 
     @classmethod
     def _import(cls, data, key=None):
@@ -128,10 +184,16 @@ class BlockchainObjects(list, Caching):
 
     # legacy
     def cache(self, key):
+        """ (legacy) store the current object with key ``key``.
+        """
         self.store(self, key)
 
 
 class BlockchainObject(dict, Caching):
+    """ This class deals with objects from graphene-based blockchains.
+        It is used to validate object ids, store entire objects in
+        the cache and deal with indexing thereof.
+    """
 
     space_id = 1
     type_id = None
@@ -185,11 +247,35 @@ class BlockchainObject(dict, Caching):
             self._store_item()
 
     def store(self, data, key="id"):
+        """ Cache the list
+
+            :param list data: List of objects to cache
+        """
         dict.__init__(self, data)
         self._store_item(key)
 
+    @classmethod
+    def cache_object(cls, data, key=None):
+        """ This classmethod allows to feed an object into the
+            cache is is mostly used for testing
+        """
+        return cls._import(cls, data, key)
+
+    @classmethod
+    def _import(cls, data, key=None):
+        c = cls(key, refresh=False)
+        c.store(data, key)
+        return c
+
     @staticmethod
     def objectid_valid(i):
+        """ Test if a string looks like a regular object id of the
+            form:::
+
+               xxxx.yyyyy.zzzz
+
+            with those being numbers.
+        """
         if "." not in i:
             return False
         parts = i.split(".")
@@ -202,9 +288,14 @@ class BlockchainObject(dict, Caching):
             return False
 
     def test_valid_objectid(self, i):
+        """ Alias for objectid_valid
+        """
         return self.objectid_valid(i)
 
     def testid(self, id):
+        """ In contrast to validity, this method tests if the objectid
+            matches the type_id provided in self.type_id or self.type_ids
+        """
         parts = id.split(".")
         if not self.type_id:
             return
@@ -221,7 +312,13 @@ class BlockchainObject(dict, Caching):
 
 
 class Object(BlockchainObject, AbstractBlockchainInstanceProvider):
+    """ This class is a basic class that allows to obtain any object
+        from the blockchyin by fetching it through the API
+    """
     def refresh(self):
+        """ This is the refresh method that overloads the prototype in
+            BlockchainObject.
+        """
         dict.__init__(
             self,
             self.blockchain.rpc.get_object(self.identifier),
