02829972a9a64fb09da8f27376ed486f5ff2c6dc
Fabian Schuh <xeroc@chainsquad.com>
2019-10-04 15:31:03 +0200

    this method is no longer available

--- tests/test_tapos_params.py
+++ tests/test_tapos_params.py
@@ -1,12 +1,12 @@
-# -*- coding: utf-8 -*-
-import unittest
-from .fixtures import Chain
-from datetime import datetime
-from graphenebase.transactions import getBlockParams
-
-
-class Testcases(unittest.TestCase):
-    def test_checkparamformating(self):
-        chain = Chain()
-        params = getBlockParams(chain.rpc)
-        self.assertEqual(params, (53066, 3847813018))
+# # -*- coding: utf-8 -*-
+# import unittest
+# from .fixtures import Chain
+# from datetime import datetime
+# from graphenebase.transactions import getBlockParams
+# 
+# 
+# class Testcases(unittest.TestCase):
+#     def test_checkparamformating(self):
+#         chain = Chain()
+#         params = getBlockParams(chain.rpc)
+#         self.assertEqual(params, (53066, 3847813018))
