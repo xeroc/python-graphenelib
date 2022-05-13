dfd0c833963d7d10febbc1e5dd4eae2e9a6d4323
Fabian Schuh <mail@xeroc.org>
2022-05-13 14:20:09 +0200

    ci: Integrate semantic-release

--- Makefile
+++ Makefile
@@ -45,31 +45,3 @@ upload:
 docs:
 	sphinx-apidoc -d 6 -e -f -o docs . *.py tests
 	make -C docs clean html
-
-docs_store:
-	git add docs
-	-git commit -m "Updating docs/"
-
-authors:
-	git shortlog -e -s -n > AUTHORS
-
-authors_store:
-	git add AUTHORS
-	-git commit -m "Updating Authors"
-
-semver: semver-release semver-updates
-
-semver-release:
-	-semversioner release
-
-semver-updates:
-	semversioner changelog > CHANGELOG.md
-	$(eval CURRENT_VERSION = $(shell semversioner current-version))
-	sed -i "s/^__version__.*/__version__ = \"$(CURRENT_VERSION)\"/" setup.py
-	-git add .semversioner setup.py CHANGELOG.md
-	-git commit -m "semversioner release updates" --no-verify
-	-git flow release start $(CURRENT_VERSION)
-	git flow release finish $(CURRENT_VERSION)
-
-prerelease: test docs docs_store authors authors_store
-release: prerelease semver clean build check dist upload git
--- setup.py
+++ setup.py
@@ -1,41 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
 
 __version__ = "1.5.2"
-URL = "https://github.com/xeroc/python-graphenelib"
 
 setup(
-    name="graphenelib",
     version=__version__,
-    description="Python library for graphene-based blockchains",
-    long_description=open("README.md").read(),
-    download_url="{}/tarball/{}".format(URL, __version__),
-    author="Fabian Schuh",
-    author_email="Fabian@chainsquad.com",
-    maintainer="Fabian Schuh",
-    maintainer_email="Fabian@chainsquad.com",
-    url=URL,
-    keywords=["graphene", "api", "rpc", "ecdsa", "secp256k1"],
-    packages=[
-        "grapheneapi",
-        "graphenebase",
-        "graphenestorage",
-        "graphenecommon",
-        "grapheneapi.aio",
-        "graphenecommon.aio",
-    ],
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-    ],
     install_requires=open("requirements.txt").readlines(),
-    extras_require={"speedups": ["secp256k1>=0.13.2"]},
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest"],
-    include_package_data=True,
+    tests_require=open("requirements-test.txt").readlines(),
+    include_package_data=True,  # needed for data from manifest
+    # Use git repo data (latest tag, current commit hash, etc) for building a
+    # version number according PEP 440. Conflicts with semantic-release
+    setuptools_git_versioning={
+        "enabled": False,
+    },
 )
